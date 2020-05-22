08d41f9767e807f1fe3a3a9955daa58c7268a216
Nabeel Valapra <nabeelvalapra@gamil.com>
2020-05-22 13:25:20 +0530

    Added scroll-memory

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router'
 import { ConnectedRouter } from 'connected-react-router'
+import ScrollMemory from 'react-router-scroll-memory';
 
 // import requireAuth from './common/requireAuth';
 import HomeContainer from './home/HomeContainer';
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <ConnectedRouter history={this.props.history}>
+          <ScrollMemory/>
           <Switch>
             <Route exact path='/' component={HomeContainer} />
             {/* <Route path='/product/:productSlug/' component={requireAuth(ProductContainer)}/> */}
